ac8808ff1bad8ab27ad0589eafbaf4bbe8c60db6
ecaskito <ecaskito@gmail.com>
2015-02-10 15:38:59 +0100

    20150211_6

--- plugins/cordova.plugins.diagnostic/src/android/Diagnostic.java
+++ plugins/cordova.plugins.diagnostic/src/android/Diagnostic.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package com.collectme.cordova.diagnostic;
+package cordova.plugins.diagnostic;
 
 import java.util.TimeZone;
 
